01d9fe5f44599a34c2d8840ab09028f453095a60
1160300613 <15244669318@163.com>
2019-05-05 22:11:27 +0800

    finish extmem

--- Databaselab4/src/extmem/Buffer.java
+++ Databaselab4/src/extmem/Buffer.java
@@ -1,5 +1,11 @@
 package extmem;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import com.google.common.primitives.UnsignedBytes;
 
 public class Buffer {
@@ -28,7 +34,7 @@ public class Buffer {
 		this.numallblock=bufsize/(blksize+1);
 		this.numfreeblk=this.numallblock;
 		data=new Block[numallblock];
-		for(int i=0;i<numallblock;i++) {
+		for(int i=0;i<numallblock;i++) { 
 			data[i]=new Block(this.blksize, 0);
 		}
 	}
@@ -48,7 +54,56 @@ public class Buffer {
 	}
 	public void freeBlockinBuffer(int i) {
 		this.numfreeblk+=1;
-		data[i].setNotAvailable();
+		data[i].setAvailable();
+	}
+	public boolean dropblockondisk(long addr) {
+		addr=addr&0xFFFFFFFF;
+		File file =new File("src/blocks/"+addr+".blk");
+		if(!file.exists()) {
+			System.out.println("File doesn't exists!");
+			return false;
+		}
+		file.delete();
+		return true;
+	}
+	public int readblockfromdisk(long addr) throws IOException {
+		addr=addr&0xFFFFFFFF;
+		File file =new File("src/blocks/"+addr+".blk");
+		if(this.numfreeblk==0) {
+			System.out.println("Buffer Overflows!");
+			return -1;
+		}
+		int result=0;
+		for(int i=0;i<this.data.length;i++) {
+			if(data[i].isAvailable()) {
+				result=i;
+				break;
+			}
+		}
+		data[result].setNotAvailable();
+		List<String>readlines=Files.readLines(file, Charsets.UTF_8);
+		byte[] resultdata=new byte[this.blksize];
+		for(int i=0;i<readlines.size();i++) {
+			resultdata[i]=Byte.parseByte(readlines.get(i));
+		}
+		data[result].setData(resultdata);
+		this.numfreeblk-=1;
+		this.numIO+=1;
+		return result;
+	}
+	public boolean writeblocktodisk(int blkptr,long addr) {
+		addr=addr&0xFFFFFFFF;
+		File file =new File("src/blocks/"+addr+".blk");
+		StringBuffer sb=new StringBuffer();
+		for(int i=0;i<this.blksize;i++) {
+			sb.append(this.data[blkptr].getData()[i]);
+			sb.append("\n");
+		}
+		this.data[blkptr].setAvailable();
+		this.numfreeblk+=1;
+		this.numIO+=1;
+		return true;
+		
 	}
 	
 }
