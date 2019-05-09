3f51cffbb3228aabf56bcb7c20e6044ee3cc13a5
1160300613 <15244669318@163.com>
2019-05-09 22:26:44 +0800

    S排序

---
+++ Databaselab4/src/projection/Projection.java
@@ -0,0 +1,12 @@
+package projection;
+
+public class Projection {
+	public static final int R_addr=0x22222222;
+	public static final int S_addr=0x77777777;
+	public static final int R_result_addr=0x11111111;
+	public static final int S_result_addr=0x33333333;
+	public static void R_projection(boolean flag) {
+		
+	}
+
+}
